7ff8edd086dc6e873b894cd1f12c45f03c99c8d9
toddnguyen47 <todd.nguyen47@gmail.com>
2018-11-11 02:00:44 -0800

    changed orders of command line args

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import core.label_graphs as label_graphs
 import params
 import sys
 
-valid_arguments = ["collapse", "saccade_calc", "label_graphs"]
+valid_arguments = ["collapse", "label_graphs", "saccade_calc"]
 
 
 def print_error():
@@ -23,12 +23,12 @@ if __name__ == "__main__":
         # collapse argument
         if argument == valid_arguments[0]:
             fixations.collapse_to_fixations(params.INPUT_CSV_FILENAME)
-        # saccade_calc
-        elif argument == valid_arguments[1]:
-            calculations.calculate(params.COLLAPSED_CSV_FILENAME)
         # label graphs
-        elif argument == valid_arguments[2]:
+        elif argument == valid_arguments[1]:
             label_graphs.read_in_file(params.COLLAPSED_FILE_TO_LABEL)
+        # saccade_calc
+        elif argument == valid_arguments[2]:
+            calculations.calculate(params.COLLAPSED_CSV_FILENAME)
         else:
             print("Invalid command line argument.")
             print_error()
